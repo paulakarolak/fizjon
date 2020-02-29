825ea13cc0fbe5c2a56a6797724d0d2b5e3202fe
paulelle <paula@karolak.pl>
2020-02-29 11:48:12 +0100

    added animations on main page

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const burgerNav = document.querySelector(".page-menu-list");
 const burgerMenu = document.querySelector(".menu-burger");
 const button = document.createElement("BUTTON");
 
-burgerMenu.addEventListener("click", function (e){
+burgerMenu.addEventListener("click", function (e) {
     e.preventDefault()
     if (burgerNav.classList.contains("show")) {
         burgerNav.classList.remove("show");
@@ -15,7 +15,7 @@ burgerMenu.addEventListener("click", function (e){
     }
 });
 
-button.addEventListener("click", function (e){
+button.addEventListener("click", function (e) {
     e.preventDefault()
     if (burgerNav.classList.contains("show")) {
         burgerNav.classList.remove("show");
@@ -27,46 +27,46 @@ button.addEventListener("click", function (e){
 const socialMedia = document.getElementById("social-media");
 const header = document.getElementById("page-header");
 console.log(socialMedia);
-window.addEventListener("load", function (){
+window.addEventListener("load", function () {
     if (window.innerWidth < 400) {
-        header.removeChild(socialMedia);
+        socialMedia.style.display = "none";
+    }
+});
+window.addEventListener("resize", function () {
+    if (window.innerWidth < 400) {
+        socialMedia.style.display = "none";
+    } if (window.innerWidth > 400) {
+        socialMedia.style.display = "flex";
     }
 });
 
-window.addEventListener("resize", function (){
-    if (window.innerWidth < 400) {
-        header.removeChild(socialMedia);
-    }
-});
- 
 // Scroll to the next div using scroll button
 
 const scrollButton = document.getElementById("btn-scroll-down");
 const myOffer = document.getElementById("my-offer");
 console.log(scrollButton);
 
-scrollButton.addEventListener("click", function (e){
+scrollButton.addEventListener("click", function (e) {
     e.preventDefault()
-    myOffer.scrollIntoView({behavior: 'smooth'})
+    myOffer.scrollIntoView({ behavior: 'smooth' })
 });
 
 // Show button to get you back to top of the website
 const topButton = document.getElementById("btn-to-top");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    topButton.style.display = "block";
-  } else {
-    topButton.style.display = "none";
-  }
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
 }
-topButton.addEventListener("click", function (e){
+topButton.addEventListener("click", function (e) {
     e.preventDefault();
     window.scroll({
-        top: 0, 
-        left: 0, 
+        top: 0,
+        left: 0,
         behavior: 'smooth'
     });
 });
-

7fc51dd1bcef1a491d176d5991203e329800e338
paulelle <paula@karolak.pl>
2020-03-01 15:11:01 +0100

    added gallery

---
+++ js/gallery.js
@@ -0,0 +1,15 @@
+// Gallery slider
+
+$(document).ready(function () {
+    $('[data-fancybox]').fancybox({
+        transitionEffect: "slide",
+        buttons: [
+            "zoom",
+            "slideShow",
+            "fullScreen",
+            "thumbs",
+            "close"
+          ],
+    });
+    
+});
